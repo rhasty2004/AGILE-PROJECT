1c365cd1712369f4919c3b9c4bc745b4196a39dc
Raymond Hasty <rhasty@clemson.edu>
2025-08-28 15:11:58 -0400

    classes.h

---
+++ classes.h
@@ -0,0 +1,15 @@
+#ifndef CLASSES_H
+#define CLASSES_H
+
+#define NUM_COURSES 16
+
+typedef struct {
+    int num;            // unique ID number for the course
+    char course[10];    // course code
+    char title[50];     // course title
+    char section[5];    // section number
+    char days[5];       // days of week
+    char time[20];      // meeting time
+} Course;
+
+#endif //
