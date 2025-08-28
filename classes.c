36211e66e1d55ecd69ece6962557ad2a3ea1d010
camallen03 <cda3@clemson.edu>
2025-08-28 17:57:17 -0400

    progress on helper functions

--- classes.c
+++ classes.c
@@ -1,46 +1,23 @@
 #include <stdio.h>
+#include "classes.h"
 
 #define NUM_COURSES 16
 
-typedef struct {
-    int num;            // unique ID number for the course
-    char course[10];    // course code
-    char title[50];     // course title
-    char section[5];    // section number
-    char days[5];       // days of week
-    char time[20];      // meeting time
-} Course;
-
-int main() {
-    Course courses[NUM_COURSES] = {
-        {1,  "CPSC 2120", "Data Structures", "001", "MWF", "10:10–11:00 AM"},
-        {2,  "CPSC 2120", "Data Structures", "002", "TR",  "1:30–2:45 PM"},
-        {3,  "CPSC 3600", "Networks", "001", "MWF", "11:15–12:05 PM"},
-        {4,  "CPSC 3600", "Networks", "002", "TR",  "9:30–10:45 AM"},
-        {5,  "CPSC 2311", "Intro to Comp Systems", "001", "MWF", "9:05–9:55 AM"},
-        {6,  "CPSC 2311", "Intro to Comp Systems", "002", "TR",  "11:00–12:15 PM"},
-        {7,  "MATH 2080", "Linear Algebra", "001", "MWF", "1:25–2:15 PM"},
-        {8,  "MATH 2080", "Linear Algebra", "002", "TR",  "12:30–1:45 PM"},
-        {9,  "MATH 2060", "Calculus III", "001", "MWF", "8:00–8:50 AM"},
-        {10, "MATH 2060", "Calculus III", "002", "TR",  "2:00–3:15 PM"},
-        {11, "ECE 2010",  "Logic & Computing Devices", "001", "MWF", "12:20–1:10 PM"},
-        {12, "ECE 2010",  "Logic & Computing Devices", "002", "TR",  "3:30–4:45 PM"},
-        {13, "ENGL 1030", "Composition & Rhetoric", "001", "MWF", "10:10–11:00 AM"},
-        {14, "ENGL 1030", "Composition & Rhetoric", "002", "TR",  "8:00–9:15 AM"},
-        {15, "PHYS 1220", "Physics w/ Calculus I", "001", "MWF", "2:30–3:20 PM"},
-        {16, "PHYS 1220", "Physics w/ Calculus I", "002", "TR",  "5:00–6:15 PM"}
-    };
-
-    // Example: print all courses
-    for (int i = 0; i < NUM_COURSES; i++) {
-        printf("%d. %s (%s) - Section %s, %s %s\n",
-               courses[i].num,
-               courses[i].course,
-               courses[i].title,
-               courses[i].section,
-               courses[i].days,
-               courses[i].time);
-    }
-
-    return 0;
-}
+Course courses[NUM_COURSES] = {
+    {1,  "CPSC 2120", "Data Structures", "001", "MWF", "10:10–11:00 AM"},
+    {2,  "CPSC 2120", "Data Structures", "002", "TR",  "1:30–2:45 PM"},
+    {3,  "CPSC 3600", "Networks", "001", "MWF", "11:15–12:05 PM"},
+    {4,  "CPSC 3600", "Networks", "002", "TR",  "9:30–10:45 AM"},
+    {5,  "CPSC 2311", "Intro to Comp Systems", "001", "MWF", "9:05–9:55 AM"},
+    {6,  "CPSC 2311", "Intro to Comp Systems", "002", "TR",  "11:00–12:15 PM"},
+    {7,  "MATH 2080", "Linear Algebra", "001", "MWF", "1:25–2:15 PM"},
+    {8,  "MATH 2080", "Linear Algebra", "002", "TR",  "12:30–1:45 PM"},
+    {9,  "MATH 2060", "Calculus III", "001", "MWF", "8:00–8:50 AM"},
+    {10, "MATH 2060", "Calculus III", "002", "TR",  "2:00–3:15 PM"},
+    {11, "ECE 2010",  "Logic & Computing Devices", "001", "MWF", "12:20–1:10 PM"},
+    {12, "ECE 2010",  "Logic & Computing Devices", "002", "TR",  "3:30–4:45 PM"},
+    {13, "ENGL 1030", "Composition & Rhetoric", "001", "MWF", "10:10–11:00 AM"},
+    {14, "ENGL 1030", "Composition & Rhetoric", "002", "TR",  "8:00–9:15 AM"},
+    {15, "PHYS 1220", "Physics w/ Calculus I", "001", "MWF", "2:30–3:20 PM"},
+    {16, "PHYS 1220", "Physics w/ Calculus I", "002", "TR",  "5:00–6:15 PM"}
+};
--- helper.c
+++ helper.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "classes.c"
+
+typedef struct {
+    char name[50];
+    char major[30];
+    int enrolled[NUM_COURSES];  // store chosen course numbers
+    int enrolledCount;
+} Profile;
+
+Profile userProfile = {"", "", {0}, 0};
 
 int searchByCourse() {
     printf("Searching by course...\n");
@@ -9,4 +19,27 @@ int searchByCourse() {
 
     
     return 0;
+}
+
+void createProfile() {
+    int choice;
+    printf("Would you like to create a profile or edit your existing one?\n");
+    printf("Enter 1 to create, or 2 to edit.\n");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        printf("Enter a name for your profile.\n");
+        scanf("%s", userProfile.name);
+        printf("Enter the name of your major.\n");
+        scanf("%s", userProfile.major);
+        for (int i = 0; i < 16; i++) {
+        printf("%d. %s (%s) - Section %s, %s %s\n",
+               courses[i].num,
+               courses[i].course,
+               courses[i].title,
+               courses[i].section,
+               courses[i].days,
+               courses[i].time);
+        }
+    }
 }
