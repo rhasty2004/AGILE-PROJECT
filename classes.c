e66c6702be6232bdda3155b40b56f3b2cc9491ca
Raymond Hasty <rhasty@clemson.edu>
2025-08-29 09:50:31 -0400

    fixing the struct in classes

--- classes.c
+++ classes.c
@@ -2,14 +2,14 @@
 #include "classes.h"
 #define NUM_COURSES 16
 
-typedef struct {
-    int num;            // unique ID number for the course
-    char course[10];    // course code
-    char title[50];     // course title
-    char section[5];    // section number
-    char days[5];       // days of week
-    char time[20];      // meeting time
-} Course;
+//typedef struct {
+  //  int num;            // unique ID number for the course
+    //char course[10];    // course code
+    //char title[50];     // course title
+    //char section[5];    // section number
+    //char days[5];       // days of week
+    //char time[20];      // meeting time
+//} Course;
 
 int main() {
     Course courses[NUM_COURSES] = {
--- helper.c
+++ helper.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "classes.c"
+#include "classes.h"
 
 typedef struct {
     char name[50];
